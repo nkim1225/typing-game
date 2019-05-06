268c444e7e95aaf5e1173d241469e830a5ec2afc
nkim1225 <nkim1225@gmail.com>
2019-05-06 19:33:42 -0400

    switching branches

--- app/serializers/Enemy_Serializer.rb
+++ app/serializers/Enemy_Serializer.rb
@@ -4,7 +4,8 @@ class EnemySerializer < ActiveModel::Serializer
   attributes :id, :name, :value, :key, :word
 
   def word
-    return RandomWord.adjs(not_shorter_than: 3, not_longer_than: 6).next
+    binding.pry
+    RandomWord.adjs(not_shorter_than: 3, not_longer_than: 6).next
   end
 
 end
