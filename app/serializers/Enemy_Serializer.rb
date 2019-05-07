d944b3941bf0706588a8bd5242e02fd20fbe3e42
nkim1225 <nkim1225@gmail.com>
2019-05-06 21:46:12 -0400

    fixed word randomizer

--- app/serializers/Enemy_Serializer.rb
+++ app/serializers/Enemy_Serializer.rb
@@ -1,11 +1,10 @@
-
-
 class EnemySerializer < ActiveModel::Serializer
   attributes :id, :name, :value, :key, :word
 
-  def word
-    binding.pry
-    RandomWord.adjs(not_shorter_than: 3, not_longer_than: 6).next
-  end
+
+  # def word
+  #   $randomWord.next
+  #   $randomWord.close
+  # end
 
 end
